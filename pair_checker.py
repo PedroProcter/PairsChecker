a43a9a6624cbe85ab54b2bc6723509e987d1e3fe
PedroProcter <OProcter89@gmail.com>
2021-09-24 10:45:48 -0400

    Refactor pair_checker.py
    -Delete unnecessary imports
    -Delete unnecessary tuple -> pairs_tuple
    -Add necessary comments

--- pair_checker.py
+++ pair_checker.py
@@ -1,12 +1,16 @@
-import re
+"""
+    Author: Pedro Procter
+    Goal: Check if all the pairs in a string are written and arranged correctly
+"""
 
-
+#Return True if all the pairs in the string are written and arranged correctly other ways it return False
 def check_pairs(target_string:str):
-    pairs_tuple = ("()", "[]", "{}")
-
+    #Replace in each step the pairs that are complete 
     for char in target_string:
+        #Checks if the string is empty (This means that everything was correct)
         if target_string == "":
             return True
+        #Replaces the pairs it encounter with and empty string
         else:
             target_string = target_string.replace("()", "")
             target_string = target_string.replace("[]", "")
@@ -16,7 +20,7 @@ def check_pairs(target_string:str):
 
 
 def main():
-    print(check_pairs("({}){{}}[][][]["))
+    print(check_pairs("({}){{}}[][][][]"))
 
 if __name__ == '__main__':
     main()
